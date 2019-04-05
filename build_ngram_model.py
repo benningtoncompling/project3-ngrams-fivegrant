88d155894f4ace828bc461bd3feb874a62281ba9
Five Grant <5@fivegrant.com>
2019-04-05 03:34:55 -0400

    im broken

--- build_ngram_model.py
+++ build_ngram_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import math, sys
+import math, sys, pprint
 
 class Corpus:
     def __init__(self, material):
@@ -20,13 +20,12 @@ class Corpus:
                 for unigram in content:
                     if unigram in unigrams:
                         unigrams[unigram][0] += 1
-                        token_collect[1] += 1
                     else:
                         unigrams.update({unigram : [1, 0]}) #, unigram]})
                         token_collect[0] += 1
-                        token_collect[1] += 1
 
             # For Probability
+            token_collect[1] = len(self.tokens)
             for unigram in unigrams:
                 unigrams[unigram][1] = \
                  unigrams[unigram][0]/token_collect[1]
@@ -41,18 +40,19 @@ class Corpus:
             #Generates N-grams where count > 1
             token_collect = [0, 0]
             ngram_list = [] 
-            for start in range(len(self.tokens) - (count - 1)):
+            for start in range(len(self.tokens) - count - 1):
                 ngram = []
                 for index_iter in range(count):
                     ngram += [self.tokens[start + index_iter]]
                 if not ('<\s>' in ngram and '<\s>' != ngram[-1]): 
                     ngram_list.append(ngram)
-                    token_collect[1] += 1
             self.ngrams.update({count : ngram_list})
+            token_collect[1] = len(ngram_list)
             
-            #Calculates Counts and Probabilities
+            #Calculates Counts
             counts = {}
-            sample = self.ngrams[count]
+            #didnt wanna break code + understanding when name is changed
+            sample = ngram_list 
             for ngram in sample:
                 title = ' '.join(ngram[:-1])
                 if title in counts:
@@ -61,16 +61,21 @@ class Corpus:
                     else:
                         counts[title].update({ngram[0]:[1,0, 
                          ' '.join(ngram)]})
-                    total = 0 
-                    for key, info in counts[title].items():
-                        total += info[0]
-                    for key, info in counts[title].items():
-                        counts[title][ngram[0]][1] = \
-                         counts[title][ngram[0]][0]/total  
+                        token_collect[0] += 1
                 else:
                     counts.update({title:{ngram[0]:[1, 1, 
                      ' '.join(ngram)]}})
                     token_collect[0] += 1
+
+            #Probabilities
+            for ngram in counts:
+                total = 0 
+                for key, info in counts[ngram].items():
+                    total += info[0]
+                for key, info in counts[ngram].items():
+                    counts[ngram][key][1] = \
+                     counts[ngram][key][0]/total 
+
             self.token_count.update({count : token_collect})
             self.ngrams_count.update({count : counts})
 
@@ -86,23 +91,35 @@ class Corpus:
             for ngram, values in self.ngrams_count[nselect].items():
                 if nselect != 1:
                     for pos in self.ngrams_count[nselect][ngram]:
-                        cell = str(values[pos][1]) + ' '
-                        cell += str(values[pos][0]) + ' '
-                        cell += str(math.log(values[pos][0])) + ' '
-                        cell += str(values[pos][2]) + '\n'
+                        cell = [values[pos][0]]
+                        cell += [str(values[pos][1])]
+                        cell += [str(math.log(values[pos][1]))]
+                        cell += [str(values[pos][2])]
                         container += [cell]
                 else:
-                    cell = str(values[1]) + ' '
-                    cell += str(values[0]) + ' '
-                    cell += str(math.log(float(values[0]))) + ' '
-                    cell += str(ngram) + '\n'
+                    cell = [values[0]]
+                    cell += [str(values[1])]
+                    cell += [str(math.log(float(values[1])))]
+                    cell += [str(ngram)]
                     container += [cell]
 
-            for line in sorted(container):
-                content += line
+            present = sorted(container)
+            present.reverse()
+            for line in present:
+                line[0] = str(line[0])
+                content += ' '.join(line) + '\n'
         return content
             
 q = Corpus('./data/input/dickens_test.txt')
 q.build(2)
 q.build(3)
-print(q)
+str(q) #print(q)
+
+"""
+corp = Corpus(sys.argv[1])
+corp.build(2)
+corp.build(3)
+with open(sys.argv[2], 'w') as output:
+    output.write(corp)
+    
+"""
